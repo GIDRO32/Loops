4a6cd591b7a254ee8051d0157a42b6cee26d2977
GIDRO32 <pambossable@gmail.com>
2022-12-29 13:53:52 +0200

    Add project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        String decision = null;
+        Scanner key = new Scanner(System.in);
+        do
+        {
+            String word = null;
+            System.out.println("Type any word:");
+            word = key.next();
+            for(int i = word.length(); i > 0; i--)
+            {
+                System.out.print(word.charAt(i - 1));
+            }
+            System.out.println("\nType y to repeat the operation or else I am turning off the machine");
+            decision = key.next();
+        }
+        while(decision.equals("y"));
     }
 }
